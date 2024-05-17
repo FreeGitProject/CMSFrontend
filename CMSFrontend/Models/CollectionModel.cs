769120f31b9b152637d06de5c7c4245280c7c8f7
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2024-05-17 16:48:53 +0530

    AddEditPage
    AddEditPage

--- CMSFrontend/Models/CollectionModel.cs
+++ CMSFrontend/Models/CollectionModel.cs
@@ -7,7 +7,8 @@ namespace CMSFrontend.Models
 {
     public class CollectionModel
     {
-        public string Title { get; set; }
+		public string Id { get; set; }
+		public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         public string Link { get; set; }
